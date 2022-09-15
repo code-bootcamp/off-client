4d5994b5ff72892ac3293a0c6c37c84b40af78a7
heechul <shiningiron94@gmail.com>
2022-09-15 12:37:31 +0900

    feat: 마이페이지 내정보 조회

--- src/components/units/info/my/myPage/myPage.container.tsx
+++ src/components/units/info/my/myPage/myPage.container.tsx
@@ -1,4 +1,4 @@
-import MyPageUI from "./myPage.presenter";
+import MyPageUI from "./MyPage.presenter";
 
 export default function MyPageContainer() {
     return<MyPageUI/>
--- src/components/units/info/my/myPage/myPage.queries.ts
+++ src/components/units/info/my/myPage/myPage.queries.ts
@@ -1,16 +1,5 @@
 import { gql } from "@apollo/client";
 
-export const FETCH_USER_LOGGED_IN = gql`
-    query fetchUserLoggedIn{
-        fetchUserLoggedIn{
-            id
-            name
-            email
-            nickname
-            phone
-        }
-    }
-`
 export const FETCH_FRIDGE_FOODS = gql`
     query fetchFridgeFoods($page: Int, $status: String){
         fetchFridgeFoods(page: $page, status: $statue){
